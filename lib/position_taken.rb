dfb0b3ed043674fb8aa1c57d8bef214a2a9852b9
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-04-27 05:55:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 #square does not need to be -1 has already happened
 def position_taken?(board, square)
-  if (board[square] == )
-  
+  if (board[square] == "" || board[square] == " ")
+    return false
+  else
+    return true
 end
