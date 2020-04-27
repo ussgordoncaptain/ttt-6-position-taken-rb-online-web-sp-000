b26d98e4b9d9f792ade050a53cdc89cea21ba5c9
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-04-27 05:56:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+#square does not need to be -1 has already happened
+def position_taken?(board, square)
+  if (board[square] == "" || board[square] == " "|| board[square] == nil)
+    return false
+  else
+    return true
+  end
+end
